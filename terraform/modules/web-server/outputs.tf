07e86736dee3a5bfccbf89056dd6bb88f7901003
TC1234785 <124154536+TC1234785@users.noreply.github.com>
2025-11-07 10:17:16 -0800

    Quick edit

--- terraform/modules/web-server/outputs.tf
+++ terraform/modules/web-server/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_ip_address" {
   description = "Public IP address of the instance"
-  value       = aws_instance.this.public_ip
+  value       = aws_instance.wk10.public_ip
 }
 
 output "instance_dns_name" {
   description = "Public DNS name of the instance"
-  value       = aws_instance.this.public_dns
+  value       = aws_instance.wk10.public_dns
 }
 
 output "instance_id" {
   description = "The instance id created"
-  value       = aws_instance.this.id
+  value       = aws_instance.wk10.id
 }
